22a418ae2865b0231d954f979422e9b6e4254493
sahana178 <85974914+sahana178@users.noreply.github.com>
2022-07-06 12:15:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+menu_list_array = ["Veg Margherita Pizza","chicken pizza","hawaiian pizza", "tandoori pizza", "panner delux pizza"//add more items
+                    ];
+
+function getmenu(){
+var htmldata;
+menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+        //give the appropriate id name as display_menu  
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+        // use the sort function as - menu_list_array.sort(); 
+        var item = document.getElementById("add_item").value;
+        htmldata=""
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;		
+      }
+
+function add_top(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
+}
